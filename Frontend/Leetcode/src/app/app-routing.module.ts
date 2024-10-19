7dc0988de1f760f294f22f69c8e24ba8c32c8138
TiboMessiaen <tibo.messiaen@student.howest.be>
2024-10-19 21:58:10 +0200

    Feat: Added app routing

--- Frontend/Leetcode/src/app/app-routing.module.ts
+++ Frontend/Leetcode/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CodeProblemsComponent } from './components/code-problems/code-problems.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'code-problems', component: CodeProblemsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
