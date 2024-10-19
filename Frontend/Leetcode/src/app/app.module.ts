3d6c504f67088bd29325f1583afea6211f74f0b6
TiboMessiaen <tibo.messiaen@student.howest.be>
2024-10-19 23:28:01 +0200

    Feat: added navigation + general layout + signup form layout

--- Frontend/Leetcode/src/app/app.module.ts
+++ Frontend/Leetcode/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Materials
 import { MatButtonModule } from '@angular/material/button';
@@ -30,6 +33,7 @@ import { CodeProblemsComponent } from './components/code-problems/code-problems.
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MatButtonModule,
     MatCardModule,
@@ -37,6 +41,7 @@ import { CodeProblemsComponent } from './components/code-problems/code-problems.
     MatInputModule,
     MatListModule,
     MatToolbarModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- Frontend/Leetcode/src/app/components/signup/signup.component.ts
+++ Frontend/Leetcode/src/app/components/signup/signup.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.signupForm = this.creatFormGroup();
+  }
+
+  creatFormGroup(): FormGroup {
+    return new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+    });
+  }
+
+  signup(): void {
+    console.log(this.signupForm.value);
+  }
 }
